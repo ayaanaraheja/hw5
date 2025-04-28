9ecff3c8c9090e096eb5a71bdb806f51d3b6fb4e
Ayaana Raheja <araheja@usc.edu>
2025-04-28 06:31:05 +0000

    hw5 done

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,12 +19,17 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
+bool helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& worked,
+    size_t day
+);
 
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -32,14 +37,51 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.empty()) {
         return false;
     }
+    
     sched.clear();
-    // Add your code below
+    size_t n = avail.size(); // days
+    sched.resize(n);
+    vector<size_t> worked(avail[0].size(), 0);
 
-
-
-
+    return helper(avail, dailyNeed, maxShifts, sched, worked, 0);
 }
 
+bool helper( // usc schedule helper lol
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& worked,
+    size_t day
+)
+{
+    if (day >= avail.size()) { return true; }
+
+    if (sched[day].size() == dailyNeed) {
+        return helper(avail, dailyNeed, maxShifts, sched, worked, day + 1);
+    }
+
+    size_t numWorkers = avail[0].size();
+
+    for (size_t worker = 0; worker < numWorkers; ++worker) {
+        if (!avail[day][worker]) { continue; }
+
+        if (find(sched[day].begin(), sched[day].end(), worker) != sched[day].end()) { continue; }
+
+        if (worked[worker] >= maxShifts) { continue; }
+
+        sched[day].push_back(worker);
+        worked[worker]++;
+
+        if (helper(avail, dailyNeed, maxShifts, sched, worked, day)) {
+            return true;
+        }
+
+        worked[worker]--; // backtrack
+        sched[day].pop_back();
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -2,18 +2,21 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+#include <algorithm>
 #include <map>
 #include <set>
 #endif
-
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
-
 // Add prototypes of helper functions here
-
+void helper(
+    std::string curr,
+    const std::string& yellow_left,
+    const std::set<std::string>& dict,
+    std::set<std::string>& words_found,
+    int index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +24,70 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    std::set<std::string> words_found;
+    helper(in, floating, dict, words_found, 0);
+    return words_found;
 }
 
 // Define any helper functions here
+void helper(
+    std::string curr,
+    const std::string& yellow_left,
+    const std::set<std::string>& dict,
+    std::set<std::string>& words_found,
+    int index)
+{
+    if(index == curr.size()) {
+        if(dict.find(curr) != dict.end() && yellow_left.empty()) {
+            words_found.insert(curr); 
+        }
+            return;
+    }
+
+
+    if(curr[index] != '-') {
+        helper(curr, yellow_left, dict, words_found, index + 1);
+        return;
+    }
+
+    for(size_t i = 0; i < yellow_left.size(); ++i) {
+        char floating_char = yellow_left[i];
+        
+        string next = curr;
+        next[index] = floating_char;
+        
+        string next_yellow = yellow_left;
+        next_yellow.erase(i, 1);
+        
+        helper(next, next_yellow, dict, words_found, index + 1);
+    }
+
+    int blanks_left = 0;
+    for(size_t i = index + 1; i < curr.size(); ++i) {
+        if(curr[i] == '-') blanks_left++;
+    }
+    
+    if(yellow_left.size() <= blanks_left) {
+        for(char c = 'a'; c <= 'z'; ++c) {
+            if(yellow_left.find(c) != string::npos) { continue; }
+            string next = curr;
+            next[index] = c;
+            
+            helper(next, yellow_left, dict, words_found, index + 1);
+        }
+    }
+}
+
+//bool all_floating_used = true;
+            
+            //for(char c : floating) {
+                //if(curr.find(c) == std::string::npos) {
+                    //all_floating_used = false;
+                    //break;
+                //}
+                //}
+                
+                //if(all_floating_used) {
+                    //words_found.insert(curr);
+                //}
+            //}
